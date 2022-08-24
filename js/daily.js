74deeb875ebcf08247e0a0ad42a32420072044e1
bryanmdean9091 <bryanmdean9091@gmail.com>
2022-08-23 19:04:25 -0700

    first commit

---
+++ js/daily.js
@@ -0,0 +1,73 @@
+// const num = document.querySelector("#num");
+// const button = document.querySelector("#submit");
+
+
+// button.addEventListener("click", () => {
+// const num = parseInt.num.value;
+// if  (num % 2 == 0)
+// alert("This is an even number");
+// else 
+//     alert("This is an odd number");
+// });
+
+// function funcNum() {
+//     let num = parseInt(prompt("Please enter a number", 10));
+//     console.log(typeof num)
+//     if (!isNaN(num)) {
+//         if (num % 2 == 0) {
+//             alert(`${num} is an even number`);
+//         } else
+//             alert(`${num} is an odd number`)
+//         }
+//         else if(isNaN(num)) {
+//             alert("Please enter a valid number")
+//             funcNum()
+//         }
+// }
+// funcNum()
+
+// function arguNum(enterNumber) {
+//     return enterNumber;
+//     let numBer = arguNum(4);
+//     if (!isNaN(arguNum)) {
+//         (arguNum % 2 == 0); {
+//             alert(`${arguNum} is an even number`);
+//         } if (arguNum % 2 != 0);
+//         alert(`${arguNum} is an odd number`)
+//          alert(`${arguNum} is an odd number`);
+//     }
+// }console.log(6);
+
+// arguNum()
+
+//Take-Home
+
+// function passWord() {
+//     const pw = prompt("Please enter a password. Password must begin with a letter and be 6-20 characters long");
+//     console.log(`${pw} ${pw.length}`);
+//     if (`${pw.length}<5`) {
+//         alert("Password has been accepted");
+//     } else 
+//     alert("Password has not been accepted");
+
+// }
+// passWord()
+
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+//function pw() {
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (password.value.length < 6 || password.value.length > 20 ) {
+        messages.push('Password must be between 6-20 characters')   
+    }
+    else 
+    messages.push('Password is acceptable')
+    if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+}
+})
+//}
+//pw()
